71442a0a37e90c2d82139a7f8726475fe391db5d
YafetGetu <kngyafcr@gmail.com>
2025-12-03 00:12:28 +0300

    Add keep_alive and update requirements

--- bot.py
+++ bot.py
@@ -7,16 +7,8 @@ from datetime import datetime
 from flask import Flask
 import threading
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-threading.Thread(target=run).start()
+from keep_alive import keep_alive
+keep_alive()
 
 
 # Load .env file
